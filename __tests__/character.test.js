ab9102b90d92fad27843e856ff82d4f16afc7e39
ChrisRDavila <crossdavila@gmail.com>
2023-04-13 14:20:44 -0700

    pushed clone onto new repo

---
+++ __tests__/character.test.js
@@ -0,0 +1,164 @@
+import { Character, Monster, Battle } from '../src/js/character.js';
+
+describe ('Character', () => {
+  let character;
+
+  beforeEach(() => {
+    character = new Character("Warrior", 10, 6, 3, 150, 150, 1, 0, 0);
+  });
+
+  test ('should create a character object with assigned properties', () => {
+    // expect(character.name).toEqual("Zuko");
+    expect(character.characterClass).toEqual("Warrior");
+    expect(character.strength).toEqual(10);
+    expect(character.intelligence).toEqual(6);
+    expect(character.dexterity).toEqual(3);
+    expect(character.maxHealth).toEqual(150);
+    expect(character.currentHealth).toEqual(150);
+    expect(character.level).toEqual(1);
+    expect(character.experience).toEqual(0);
+    expect(character.money).toEqual(0);
+  });
+
+  test ('should show character moving up each level', () => {
+    character.levelUp();
+    expect(character.level).toBeGreaterThan(1);
+    expect(character.dexterity).toBeGreaterThan(3);
+    expect(character.strength).toBeGreaterThan(10);
+    expect(character.intelligence).toBeGreaterThan(6);
+    expect(character.currentHealth).toBeGreaterThan(150);
+    expect(character.maxHealth).toBeGreaterThan(150);
+  });
+});
+  
+describe ('Monster', () => {
+  let monster;
+
+  beforeEach(() => {
+    monster = new Monster("Fire Nation Soldier", 2, 2, 3, 3, 100, 100);
+  });
+
+  test ('should create a monster object with assigned properties', () => {
+    expect(monster.name).toEqual("Fire Nation Soldier");
+    expect(monster.strength).toEqual(2);
+    expect(monster.defense).toEqual(2);
+    expect(monster.maxHealth).toEqual(3);
+    expect(monster.currentHealth).toEqual(3);
+    expect(monster.experienceReward).toEqual(100);
+    expect(monster.moneyReward).toEqual(100);
+  });
+})
+
+
+describe ('Battle', () => {
+  let battle;
+
+  beforeEach(() => {
+  const character = {
+      strength: 10,
+      defense: 2,
+      currentHealth: 150,
+      experience: 0,
+      money: 0
+    };
+    const monster = {
+      strength: 3,
+      defense: 2,
+      currentHealth: 8,
+      experienceReward: 100,
+      moneyReward: 100
+    };
+  battle = new Battle(character, monster);
+  });
+  
+  test('should decrease monster health by monster strength minus character defense', () => {
+    const expectedHeroDamage= battle.character.strength - battle.monster.defense;
+    const expectedMonsterHealth = battle.monster.currentHealth - expectedHeroDamage;
+    const newExperience = battle.character.experience + battle.monster.experienceReward;
+    const newMoney = battle.character.money + battle.monster.moneyReward;
+    const expectedMonsterDamage = battle.monster.strength - battle.character.defense;
+    const expectedHeroHealth = battle.character.currentHealth - expectedMonsterDamage;
+    battle.battleAttack();
+    expect(expectedMonsterHealth).toBe(battle.monster.currentHealth);
+    expect(battle.character.experience).toBe(newExperience);
+    expect(battle.character.money).toBe(newMoney);
+    expect(expectedHeroHealth).toBeLessThan(battle.character.currentHealth);
+  });
+});
+
+describe ('Gameplay', () => {
+  let game;
+  beforeEach(() => {
+    const step = 0;
+    const map = ["start", "", "monster", "blank", "treasure", "", "monster", "end"];
+    game = new Gameplay(step, map);
+
+    test('should start a new gameplay with step value of one and assign array positions for gameplay events');
+    expect(game.step).toEqual(0);
+    expect(game.map).toEqual("start", "", "monster", "blank", "treasure", "", "monster", "end");
+  })
+})
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this.name = name;
+// this.strength = strength;
+// this.defense = defense;
+// this.maxHealth = maxHealth;
+// this.currentHealth = currentHealth;
+// this.experienceReward = experienceReward;
+// this.moneyReward = moneyReward;
+// }
+  
+
+// describe ('Game', () => {
+//   let game;
+
+//   beforeEach(() => {
+//     game = new Game();
+//   });
+  
+  // test ("should assign characterStyle to the player")
+
+
+
+
+
+  // test ('should assign id to character', () => {
+  //   const newCharacter = { name: "Kitara"};
+  //   game.addCharacter(newCharacter);
+  //   expect(newCharacter.id).toEqual(1); 
+  // }); 
+
+  // test ('should add a character to the game object', () => {
+  //   const newCharacter1 = { name: "Iroh", name: "Kitara"}; 
+  //   game.addCharacter(newCharacter1);
+  //   expect(game.characterCreation).toEqual({1: "Iroh", 2: "Jeff"});
+  // });
+
+  // test ('should assign characters to new game', () => {
+  //   const newCharacter = { name: "Kitara"};
+  //   game.addCharacter(newCharacter);
+  //   expect(game.characterCreation).toEqual({1: newCharacter});
+  // });
+
+  //   expect(game.characterCreation).toContain("Kitara", "Jeff");
+  // });
---
+++ src/js/character.js
@@ -0,0 +1,71 @@
+import 'src/css/styles.css';
+
+export class Character {
+  constructor(characterClass, strength, intelligence, dexterity, maxHealth, currentHealth, level, experience, money) {
+    this.characterClass = characterClass;
+    this.strength = strength;
+    this.intelligence = intelligence;
+    this.dexterity = dexterity;
+    this.maxHealth = maxHealth;
+    this.currentHealth = currentHealth;
+    this.level = level;
+    this.experience = experience;
+    this.money = money;
+    // this.inventory = new Inventory(10);
+  }
+
+  levelUp() {
+    this.level++;
+    this.strength += 2;
+    this.intelligence += 2;
+    this.dexterity += 2;
+    this.maxHealth += 10;
+    this.currentHealth = this.maxHealth;
+  }
+}
+
+export class Monster {
+  constructor(name, strength, defense, maxHealth, currentHealth, experienceReward, moneyReward) {
+    this.name = name;
+    this.strength = strength;
+    this.defense = defense;
+    this.maxHealth = maxHealth;
+    this.currentHealth = currentHealth;
+    this.experienceReward = experienceReward;
+    this.moneyReward = moneyReward;
+  }
+}
+
+
+export class Battle {
+  constructor(character, monster) {
+    this.character = character;
+    this.monster = monster;
+  }
+
+  battleAttack() {
+    const attackDamage = this.character.strength - this.monster.defense;
+    this.monster.currentHealth -= attackDamage;
+    if (this.monster.currentHealth <= 0) {
+      this.character.experience += this.monster.experienceReward;
+      this.character.money += this.monster.moneyReward;
+      return "You have defeated the monster.";
+    } else {
+      return "The monster has " + this.monster.currentHealth + " health remaining.";
+    }
+  }
+}
+export class Gameplay {
+  constructor(stepAmount, map) {
+    this.stepAmount = step;
+    this.map = map;
+  }
+//   newArea(){
+//     this.stepAmount += 1;
+
+//   }
+// } 
+
+
+// newArea, finishGame, triggerTreasure, triggerMonster, experience > x, increase 1 (increase health strength)
+
